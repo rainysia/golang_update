dcc93e288d5be94093f0c1f9988d19295ee650ab
rainysia <rainysia@gmail.com>
2020-05-30 22:57:16 +0800

    update to latest

--- go_update.sh
+++ go_update.sh
@@ -25,7 +25,7 @@
 # /bin/bash /home/sh/go_update.sh
 set -e
 declare -a third_packages
-golang_package='/home/softs/develop/go1.14.linux-amd64.tar.gz'
+golang_package='/home/softs/develop/go1.14.3.linux-amd64.tar.gz'
 golang_path='/usr/local/'
 third_packages_path='/usr/local/gotom/src/'
 third_packages_repo_path=(
